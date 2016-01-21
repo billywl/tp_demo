c54bf50a63f30273bb2814e153dbe874760a6bf4
billywl <183692566@qq.com>
2016-01-21 16:53:07 +0800

    first time

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+		'URL_MODEL'             =>  1,       // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
+		// 0 (普通模式); 1 (PATHINFO 模式); 2 (REWRITE  模式); 3 (兼容模式)  默认为PATHINFO 模式
+		'URL_PATHINFO_DEPR'     =>  '/',	// PATHINFO模式下，各参数之间的分割符号
+);
